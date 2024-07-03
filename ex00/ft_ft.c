5337e2f9c2c4cf83b132b84e9e07542b914cd864
José Antonio <joseantonio@Wanaldinos-MacBook-Pro.local>
2024-07-03 11:23:53 +0200

    ex00

---
+++ ex00/ft_ft.c
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+
+void ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+/*int main()
+{
+	int num;
+
+	num = 1;
+	printf("%d\n", num);
+	ft_ft(&num);
+	printf("%d\n", num);
+
+	return 0;	
+}*/
